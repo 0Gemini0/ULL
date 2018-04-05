82c0ac3f5b79f1d39d972c26736abb12e074d79a
0Gemini0 <tlpelsmaeker@gmail.com>
2018-04-05 14:06:56 +0200

    bugfix cosine

--- Practical 1/helpers.py
+++ Practical 1/helpers.py
@@ -2,9 +2,12 @@
 A file for all our (helper) functions.
 """
 import numpy as np
+from scipy.spatial.distance import cosine
+from scipy.stats import pearsonr, spearmanr
 
 
 def load_embeddings(path):
+    """Loads a file of word embeddings stored with a single embedding per line"""
     print("Loading {}...".format(path.split("/")[-1]))
     words = dict()
     with open(path, 'r', encoding='utf8') as f:
@@ -13,3 +16,10 @@ def load_embeddings(path):
             words[word[0]] = np.array(word[1:]).astype(np.float32)
     print("Done")
     return words
+
+
+def get_cosine(word_1, word_2, embeddings):
+    """Computes the cosine similarity of two words given an embedding dictionary."""
+    emb_1 = embeddings[word_1]
+    emb_2 = embeddings[word_2]
+    return 1. - cosine(emb_1, emb_2)
--- Practical 1/main.py
+++ Practical 1/main.py
@@ -3,7 +3,7 @@ This file will contain the main script.
 """
 import numpy as np
 
-from helpers import load_embeddings
+from helpers import load_embeddings, get_cosine
 
 
 def main():
@@ -11,6 +11,15 @@ def main():
     deps = load_embeddings("Embeddings/deps.words")
     bow2 = load_embeddings("Embeddings/bow2.words")
     bow5 = load_embeddings("Embeddings/bow5.words")
+
+    # Test with cosine
+    print(get_cosine('cat', 'dog', deps))
+    print(get_cosine('cat', 'dog', bow2))
+    print(get_cosine('cat', 'dog', bow5))
+
+    print(get_cosine('dog', 'dog', deps))
+    print(get_cosine('dog', 'dog', bow2))
+    print(get_cosine('dog', 'dog', bow5))
 
 
 if __name__ == '__main__':
