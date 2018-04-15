cde1f9af0458a17374aa0a367c314dcf9417ba27
0Gemini0 <tlpelsmaeker@gmail.com>
2018-04-15 17:27:02 +0200

    ex 5 code done

--- Practical 1/helpers.py
+++ Practical 1/helpers.py
@@ -7,9 +7,11 @@ from scipy.stats import pearsonr, spearmanr
 from collections import defaultdict
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, DBSCAN
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import os
+import os.path as osp
 
 
 def load_embeddings(path, verbose=True):
@@ -25,7 +27,9 @@ def load_embeddings(path, verbose=True):
 
 
 def compute_correlations(embeddings, dataset):
-    """Get the correlations between a number of embedding dictionaries and a similarity dataset"""
+    """
+    Get the correlations between a number of embedding dictionaries and a similarity dataset.
+    """
     cosines = [[] for i in range(len(embeddings))]
     scores = []
     pearson = []
@@ -52,14 +56,18 @@ def compute_correlations(embeddings, dataset):
 
 
 def get_cosine(word_1, word_2, embeddings):
-    """Computes the cosine similarity of two words given an embedding dictionary."""
+    """
+    Computes the cosine similarity of two words given an embedding dictionary.
+    """
     emb_1 = embeddings[word_1]
     emb_2 = embeddings[word_2]
     return 1. - cosine(emb_1, emb_2)
 
 
 def get_correlation(x, y):
-    """Computes the spearman and pearson correlation coëfficients of two arrays"""
+    """
+    Computes the spearman and pearson correlation coëfficients of two arrays.
+    """
     pearson = pearsonr(x, y)
     spearman = spearmanr(x, y)
     return pearson, spearman
@@ -120,35 +128,63 @@ def retrieve_word_analogy_data_dict (path_to_data):
     return data_required
 
 
-def reduce_dimensions(embeddings, dim, t_dim=None, t_num=5000, verbose=True):
+def reduce_dimensions(embeddings, dim, mode, verbose, t_dim=50, t_num=5000):
     """
-    Reduce the dimensions of word embeddings with PCA and TSNE
+    Reduce the dimensions of word embeddings with PCA and/or TSNE.
     Args:
-    * embeddings (dict): word embeddings with keys.
+    * embeddings (list[np.array]): set of word embeddings.
     * dim (int): final dimension after reduction.
-    * t_dim (int): if not none, embeddings are compressed to t_dim before tsne.
+    * t_dim (int): if not none, embeddings are compressed to t_dim before T-SNE.
     * t_num (int): maximum number of datapoints for T-SNE.
-    * verbose: whether to display prints.
+    * verbose (bool): whether to display prints.
+    * pca (bool): whether to run PCA.
+    * tsne (bool): whether to run T-SNE.
     """
-
-    # Define dimension reduction algorithms
-    pca = PCA(n_components=dim, whiten=True)
-    pca_tsne = PCA(n_components=t_dim, whiten=True)
-    tsne = TSNE(n_components=dim, verbose=verbose, n_iter=10000, perplexity=10)
-
-    # Reduce dimensions with PCA and TSNE
-    vprint("Reducing dimensions with PCA...", verbose)
-    pca_clusters = pca.fit_transform(embeddings)
-    if t_dim is not None:
-        vprint("Pre-reducing dimensions for TSNE...", verbose)
-        tsne_pre_clusters = pca_tsne.fit_transform(embeddings)
-    else:
-        tsne_pre_clusters = embeddings
-    vprint("Reducing dimensions with TSNE...", verbose)
-    tsne_clusters = tsne.fit_transform(tsne_pre_clusters[:t_num, :])
+    clusters = []
+    for embedding in embeddings:
+        if mode == 'pca':
+            vprint("Reducing dimensions with PCA...", verbose)
+            clusters.append(reduce_dimensions_pca(embedding, dim, verbose))
+        elif mode == 'tsne':
+            vprint("Reducing dimensions with TSNE...", verbose)
+            clusters.append(reduce_dimensions_tsne(embedding[:t_num, :], dim, t_dim, verbose))
     vprint("Done", verbose)
+    return clusters
 
-    return pca_clusters, tsne_clusters
+
+def reduce_dimensions_pca(embeddings, dim, verbose):
+    pca = PCA(n_components=dim, whiten=True)
+    clusters = pca.fit_transform(embeddings)
+    return
+
+
+def reduce_dimensions_tsne(embeddings, dim, t_dim, verbose):
+    tsne = TSNE(n_components=dim, verbose=verbose, n_iter=5000, perplexity=30)
+    if t_dim is not None:
+        pca = PCA(n_components=t_dim, whiten=True)
+        pre_clusters = pca.fit_transform(embeddings)
+    else:
+        pre_clusters = embeddings
+    clusters = tsne.fit_transform(pre_clusters)
+    return clusters
+
+
+def visualize_embeddings(embeddings, labels, titles, num, dim, verbose):
+    """
+    Visualize a list of dimension-reduced embeddings.
+    """
+    if not labels:
+        labels = [np.ones(num)] * len(embeddings)
+    for embedding, label, title in zip(embeddings, labels, titles):
+        if dim == 2:
+            print(embedding.shape)
+            print(label.shape)
+            print(title)
+            visualize_embeddings_2d(embedding, label, title, num)
+        elif dim == 3:
+            visualize_embeddings_3d(embedding, label, title, num)
+        else:
+            vprint("Cannot visualize in {} dimensional space".format(dim), verbose)
 
 
 def visualize_embeddings_2d(embeddings, labels, title, num):
@@ -163,6 +199,9 @@ def visualize_embeddings_2d(embeddings, labels, title, num):
 
 
 def visualize_embeddings_3d(embeddings, labels, title, num):
+    """
+    Visualize 3-D compressed embeddings.
+    """
     fig = plt.figure(figsize=(4,3))
     ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
     ax.scatter(embeddings[:num, 0], embeddings[:num, 1], embeddings[:num, 2], c=labels[:num].astype(float))
@@ -170,17 +209,55 @@ def visualize_embeddings_3d(embeddings, labels, title, num):
     ax.set_ylabel("dim_2")
     ax.set_zlabel("dim_3")
     ax.set_title(title)
-
     plt.show()
 
 
-def cluster(embeddings, k):
+def cluster(embeddings, mode, verbose, k=2, eps=0.1, min_samples=4):
     """
-    Cluster a group of nouns based on their embeddings.
+    Cluster a set of embeddings with a method of choice.
+    """
+    labels = []
+    for i, embedding in enumerate(embeddings):
+        if mode == 'distance':
+            vprint("Clustering set {} with a distance metric (K-means)...".format(i), verbose)
+            labels.append(cluster_distance(embedding, k))
+        elif mode == 'density':
+            vprint("Clustering set {} with a density metric (DBSCAN)...".format(i), verbose)
+            labels.append(cluster_density(embedding, eps, min_samples))
+        else:
+            raise Exception("Unknown clustering method, select [distance, density]")
+    vprint("Done", verbose)
+    return labels
+
+
+def cluster_distance(embeddings, k):
+    """
+    Cluster a group of nouns based on their embeddings with a distance metric.
     """
     kmeans = KMeans(n_clusters=k, random_state=0)
     labels = kmeans.fit_predict(embeddings)
     return labels
+
+
+def cluster_density(embeddings, eps, min_samples):
+    """
+    Cluster a group of nouns based on their embeddings with a density metric.
+    """
+    dbscan = DBSCAN(min_samples=min_samples, eps=eps, metric='cosine')
+    labels = dbscan.fit_predict(embeddings)
+    return labels
+
+def save_clusters(nouns, labels, path, names):
+    """
+    Write clusters to a file for later inspection.
+    """
+    if not osp.isdir(path):
+        os.makedirs(path)
+
+    for label, name in zip(labels, names):
+        with open(osp.join(path, name), 'w', encoding='utf8') as f:
+            for i in range(max(label)):
+                f.write("Cluster {}: {}".format(i, [nouns[j] for j in list(np.argwhere(label == i).flatten())]))
 
 
 def vprint(message, verbose=True):
--- Practical 1/main.py
+++ Practical 1/main.py
@@ -6,19 +6,18 @@ This file will contain the main script.
 import numpy as np
 from settings import Settings
 
-from helpers import load_embeddings, get_cosine, get_correlation, retrieve_SIMLEX999_data_dict, retrieve_MEN_data_dict, compute_correlations, reduce_dimensions, visualize_embeddings_2d, visualize_embeddings_3d, cluster
-
+from helpers import load_embeddings
+from helpers import retrieve_SIMLEX999_data_dict, retrieve_MEN_data_dict, compute_correlations
+from helpers import reduce_dimensions, visualize_embeddings, cluster, save_clusters
 
 def main(opt):
 
     # Load word embeddings into dictionaries
     deps = load_embeddings("Embeddings/deps.words")
-    # bow2 = load_embeddings("Embeddings/bow2.words")
-    # bow5 = load_embeddings("Embeddings/bow5.words")
+    bow2 = load_embeddings("Embeddings/bow2.words")
+    bow5 = load_embeddings("Embeddings/bow5.words")
 
-
-    if (opt.exercise == 3):
-
+    if (opt.exercise == 3 | opt.exercise == 1):
         # Load similarity dataset into dictionaries
         simlex = retrieve_SIMLEX999_data_dict("Similarities/SimLex-999.txt")
         men = retrieve_MEN_data_dict('Similarities/MEN_dataset_natural_form_full')
@@ -30,42 +29,36 @@ def main(opt):
         print("pearson: {}, spearman: {} for SimLex".format(p_sim, s_sim))
         print("pearson: {}, spearman: {} for MEN".format(p_men, s_men))
 
-    elif (opt.exercise == 4):
+    elif (opt.exercise == 4 | opt.exercise == 1):
         pass
 
-    elif (opt.exercise == 5):
+    elif (opt.exercise == 5 or opt.exercise == 1):
         # Load nouns
         with open(opt.nouns, 'r', encoding='utf8') as f:
             nouns = f.read().split()
 
-        # Return an embedding matrix ordered as the nouns list
+        # Return embedding matrices ordered as the nouns list
         deps_nouns = np.array([deps[noun] for noun in nouns])
-        print(deps_nouns.shape)
-        print(deps_nouns[1,:10], deps_nouns[2,:10])
+        bow2_nouns = np.array([bow2[noun] for noun in nouns])
+        bow5_nouns = np.array([bow5[noun] for noun in nouns])
 
-        # Reduce dimensions of deps
-        pca, tsne = reduce_dimensions(deps_nouns, opt.dim, 50, opt.tsne_num, True)
+        # Reduce dimensions of noun embeddings
+        reduced_embeddings = reduce_dimensions([deps_nouns, bow2_nouns, bow5_nouns],
+                                      opt.dim, opt.red_mode, opt.verbose, opt.tsne_dim, opt.tsne_num)
 
-        # K-means
-        pca_labels = cluster(pca, k = opt.k)
-        tsne_labels = cluster(tsne, k=opt.k)
+        # Clustering of noun embeddings
+        labels = cluster([deps_nouns, bow2_nouns, bow5_nouns],
+                         opt.clu_mode, opt.verbose, opt.k, opt.eps, opt.min_samples)
 
         # Visualize
-        if opt.dim == 2:
-            visualize_embeddings_2d(pca, pca_labels, "PCA reduced embeddings.", opt.tsne_num)
-            visualize_embeddings_2d(tsne, tsne_labels, "TSNE reduced embeddings.", opt.tsne_num)
-        elif opt.dim == 3:
-            visualize_embeddings_3d(pca, pca_labels, "PCA reduced embeddings.", opt.tsne_num)
-            visualize_embeddings_3d(tsne, tsne_labels, "TSNE reduced embeddings.", opt.tsne_num)
-        else:
-            print("Cannot visualize in {} dimensions.".format(opt.dim))
+        titles = ["{} deps embeddings with {} based cluster labels".format(opt.red_mode.capitalize(), opt.clu_mode),
+                  "{} bow2 embeddings with {} based cluster labels".format(opt.red_mode.capitalize(), opt.clu_mode),
+                  "{} bow5 embeddings with {} based cluster labels".format(opt.red_mode.capitalize(), opt.clu_mode)]
+        visualize_embeddings(reduced_embeddings, labels, titles,
+                            opt.viz_num, opt.dim, opt.verbose)
 
         # Qualitative
-        for i in range(opt.k):
-            print("Cluster {}: {}".format(i, [nouns[j] for j in list(np.argwhere(tsne_labels == i).flatten())]))
-
-
-
+        save_clusters(labels, nouns, opt.out, ['deps_clusters.txt', 'bow2_cluster.txt', 'bow5_cluster.txt'])
 
 
 if __name__ == '__main__':
