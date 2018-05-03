b8c4999ea3744badf3f68605d8ba183704f281f3
Nuno Mota <nunonetomota@gmail.com>
2018-05-03 15:43:19 +0200

    Basic dataset preprocessing done.

--- Practical 2/Code/helpers.py
+++ Practical 2/Code/helpers.py
@@ -10,82 +10,58 @@ from collections import defaultdict
 import operator
 import pickle
 # path_to_data = "../Data/Original Data/test_data_file.txt"
-path_to_data = "../Data/Original Data/hansards/training.en"
-# path_to_data = "../Data/Original Data/europarl/training.en"
+# path_to_data = "../Data/Original Data/hansards/training.en"
+path_to_data = "../Data/Original Data/europarl/training.en"
 
 #####################################
 # Data loading/processing functions #
 #####################################
 
-def drop_frequent_words(path_to_data, threshold, path_to_destination_file, lowercase=True):
-    ##################################################################################################
-    """Drops the most frequent (according to function dependent on threshold) words of the dataset."""
-    ##################################################################################################
-
+def basic_dataset_preprocess(path_to_data, threshold=10000, lowercase=True):
+    ############################################################################
+    """Keep 'threshold' most frequent words. Lowercase(?) them. UNK the rest."""
+    ############################################################################
 
     '''Load the data.'''
     with open(path_to_data, "r", encoding='utf-8') as f:
         data_lines = f.readlines()
 
-
-    total_number_of_words = 0
-    word_count = defaultdict(lambda: 0)
-    for line in data_lines:
+    def line_mutate(line):
         line = line.split(" ")
         line[-1] = line[-1].rstrip("\n").rstrip("\r")
 
+        if (lowercase):
+            line = [word.lower() for word in line]
+
         for word in line:
-            total_number_of_words += 1
-            word_count[word.lower()] += 1
+            word_count[word] += 1
 
-    '''This is an experimental "dirty" (as per Omar Levy) subsampler.'''
-    '''It appears to get very similar dropping probabilities for corpora with different sizes.'''
+        return line
+
+    word_count = defaultdict(lambda: 0)
+    data_lines = [line_mutate(line) for line in data_lines]
+
     ordered_counts = sorted(word_count.items(), key=operator.itemgetter(1), reverse=True)
-    max_count = ordered_counts[0][1]
 
-    unigram_probablities = {}
-    for word in word_count:
-        unigram_probablities[word] = word_count[word] / total_number_of_words
-
-    ordered_unigram_probabilities = sorted(unigram_probablities.items(), key=operator.itemgetter(1), reverse=True)
-
-    total_count_most_frequent_words = 0
-    words_above_threshold = []
-    for unigram_probability in ordered_unigram_probabilities:
-        if (unigram_probability[1] >= threshold):
-            total_count_most_frequent_words += word_count[unigram_probability[0]]
-            words_above_threshold.append(unigram_probability[0])
-            min_count = word_count[unigram_probability[0]]
+    to_UNK_dict = {}
+    for i, pair in enumerate(ordered_counts):
+        if (i <= threshold):
+            to_UNK_dict[pair[0]] = pair[0]
         else:
-            break
+            to_UNK_dict[pair[0]] = "UNK"
 
-    word_drop_out_prob = {}
-    for word in word_count:
-        if (word in words_above_threshold):# TODO: Maybe just set it to 1?
-            word_drop_out_prob[word] = (word_count[word] / (max_count * (1 + min_count/total_count_most_frequent_words)))**(1/10)# TODO: Find an appropriate value
-        else:
-            word_drop_out_prob[word] = 0
-
-    i = 0
-    flag = False
-    for pair in ordered_counts:
-        print("Word: " + pair[0] + " ||| Count: " + str(word_count[pair[0]]) + " ||| Dropout prob: " + str(word_drop_out_prob[pair[0]]))
-        if (word_drop_out_prob[pair[0]] >= 0.2):
-            flag = True
-        if(flag):
-            i += 1
-            if(i > 20):
-                break
-
-
-    # new_corpora = []
-    # for line in data_lines:
-    #     new_line = []
-    #     for word in line:
+    with open(path_to_data[0:-3] + "_" + str(threshold) + "_" + str(lowercase) + path_to_data[-3:], "w", encoding='utf-8') as f:
+        for line in data_lines:
+            new_line = ""
+            for word in line:
+                new_line += to_UNK_dict[word] + " "
+            f.write(new_line[0:-1] + "\n")
 
 
 
-drop_frequent_words(path_to_data, 0.005, 0)
+basic_dataset_preprocess(path_to_data)
+
+
 
 def preprocess_data_skipgram(path_to_data, window_size, k=1, store_sequentially=False):
     ###############################################################################
@@ -149,3 +125,49 @@ def preprocess_data_skipgram(path_to_data, window_size, k=1, store_sequentially=
 
 
 # preprocess_data_skipgram(path_to_data, 2)
+
+
+
+def damned_experimental_subsampler():
+    '''This is an experimental "dirty" (as per Omar Levy) subsampler.'''
+    '''It appears to get very similar dropping probabilities for corpora with different sizes.'''
+    # ordered_counts = sorted(word_count.items(), key=operator.itemgetter(1), reverse=True)
+    # max_count = ordered_counts[0][1]
+    #
+    # unigram_probablities = {}
+    # for word in word_count:
+    #     unigram_probablities[word] = word_count[word] / total_number_of_words
+    #
+    # ordered_unigram_probabilities = sorted(unigram_probablities.items(), key=operator.itemgetter(1), reverse=True)
+    #
+    # total_count_most_frequent_words = 0
+    # words_above_threshold = []
+    # for unigram_probability in ordered_unigram_probabilities:
+    #     if (unigram_probability[1] >= threshold):
+    #         total_count_most_frequent_words += word_count[unigram_probability[0]]
+    #         words_above_threshold.append(unigram_probability[0])
+    #         min_count = word_count[unigram_probability[0]]
+    #     else:
+    #         break
+    #
+    # word_drop_out_prob = {}
+    # for word in word_count:
+    #     if (word in words_above_threshold):  # TODO: Maybe just set it to 1?
+    #         word_drop_out_prob[word] = (word_count[word] / (
+    #         max_count * (1 + min_count / total_count_most_frequent_words))) ** (
+    #                                    1 / 10)  # TODO: Find an appropriate value
+    #     else:
+    #         word_drop_out_prob[word] = 0
+    #
+    # i = 0
+    # flag = False
+    # for pair in ordered_counts:
+    #     print("Word: " + pair[0] + " ||| Count: " + str(word_count[pair[0]]) + " ||| Dropout prob: " + str(
+    #         word_drop_out_prob[pair[0]]))
+    #     if (word_drop_out_prob[pair[0]] >= 0.2):
+    #         flag = True
+    #     if (flag):
+    #         i += 1
+    #         if (i > 20):
+    #             break
+    raise NotImplementedError()
