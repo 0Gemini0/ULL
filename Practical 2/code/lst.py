ba2c99a43d5dc6215537d3a4c6e72fc2d9031a15
Nuno-Mota <nunonetomota@gmail.com>
2018-05-16 23:13:19 +0200

    fixed callable error

--- Practical 2/code/lst.py
+++ Practical 2/code/lst.py
@@ -65,9 +65,7 @@ def write_lst(filename, data, scores, indices, model, idx_to_word):
 
         for score, index in zip(scores, indices):
             if model == 'embedalign':
-                print(data[0], data[0].type, data[0].shape, data[2], data[2].type, data[2].shape, index)
-                print("HELLO ", data[0][index, data[2].item()])
-                candidate = idx_to_word(int(data[0][index, data[2].item()].item()))
+                candidate = idx_to_word[int(data[0][index, data[2].item()])]
             else:
                 candidate = idx_to_word[int(data[0][index].item())]
             f.write("\t{} {}".format(candidate, score))
